65b631e0d47e67ca51a193ba6219a101cd1b883e
mkravchenko <m.kravchenko18@gmail.com>
2016-11-28 13:16:14 +0200

    changed functions names and changed 'if and else' to bool(...). Removed inner functions.

--- check_sites_health.py
+++ check_sites_health.py
@@ -12,13 +12,10 @@ def load_urls4check(path):
     return data
 
 
-def is_server_respond_with_200(url):
+def is_server_response_with_200(url):
     try:
         response = requests.get(url)
-        if response.status_code == 200:
-            return True
-        else:
-            return False
+        return bool(response.status_code == 200)
     except requests.exceptions.ConnectionError:
         return False
 
@@ -35,19 +32,35 @@ def is_domain_expiration_date_more_then_month(expiration_date):
     current_day = datetime.today()
     month_next = calendar.monthrange(current_day.year, current_day.month + 1)[1]
     diff = expiration_date - datetime.today()
-    if month_next <= diff.days:
-        return True
-    return False
+    return bool(month_next <= diff.days)
 
 
-def run_main_operations():
-    urls_list = load_urls4check(path_to_file_with_urls)
-    for url in urls_list:
-        url = url.replace("\n", "")
-        is_status_code_200 = is_server_respond_with_200(url)
-        expiration_date = get_domain_expiration_date(url)
-        is_expiration_date_more_then_month = is_domain_expiration_date_more_then_month(expiration_date)
-        print_site_info(url, is_status_code_200, is_expiration_date_more_then_month)
+def is_path_valid(path_to_file):
+    if not os.path.exists(path_to_file):
+        print("Please check input path to the file with urls.")
+        return False
+    return True
+
+
+def enter_path_to_file():
+    while True:
+        path_to_file = input("Enter path to file with urls: ")
+        is_valid = is_path_valid(path_to_file)
+        if not path_to_file:
+            exit("Program stopped.")
+        if is_valid:
+            return path_to_file
+
+
+def get_path_to_file():
+    if len(sys.argv) <= 1:
+        path = enter_path_to_file()
+    else:
+        path = sys.argv[1]
+        is_path_valid_file_path = is_path_valid(path)
+        if not is_path_valid_file_path:
+            path = enter_path_to_file()
+    return path
 
 
 def print_site_info(url, is_status_code_200, is_expiration_date_more_then_month):
@@ -61,32 +74,16 @@ def print_site_info(url, is_status_code_200, is_expiration_date_more_then_month)
         print("{0} - status code and expiration date FAILED.".format(url))
 
 
-def get_path_to_file():
-    def is_path_valid(path_to_file):
-        if not os.path.exists(path_to_file):
-            print("Please check input path to the file with urls.")
-            return False
-        return True
-
-    def enter_password():
-        while True:
-            path_to_file = input("Enter path to file with urls: ")
-            is_valid = is_path_valid(path_to_file)
-            if not path_to_file:
-                exit("Program stopped.")
-            if is_valid:
-                return path_to_file
-
-    if len(sys.argv) <= 1:
-        path = enter_password()
-    else:
-        path = sys.argv[1]
-        is_path_valid_file_path = is_path_valid(path)
-        if not is_path_valid_file_path:
-            path = enter_password()
-    return path
+def main_runner():
+    path_to_file_with_urls = get_path_to_file()
+    urls_list = load_urls4check(path_to_file_with_urls)
+    for url in urls_list:
+        url = url.rstrip()
+        is_status_code_200 = is_server_response_with_200(url)
+        expiration_date = get_domain_expiration_date(url)
+        is_expiration_date_more_then_month = is_domain_expiration_date_more_then_month(expiration_date)
+        print_site_info(url, is_status_code_200, is_expiration_date_more_then_month)
 
 
 if __name__ == '__main__':
-    path_to_file_with_urls = get_path_to_file()
-    run_main_operations()
+    main_runner()
